6c5dc4d8f37e2451c6bede544244f8ca79269b42
Miguel Angelo <m.angelodev@gmail.com>
2025-06-19 12:48:28 -0300

    Inicializa o projeto

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+        <div>
+            <h1 className={'text-2xl font-bold text-center'}>This is the app.</h1>
+        </div>
+    </>
+  )
+}
+
+export default App
